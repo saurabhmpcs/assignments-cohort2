b418cf5cc223a6b0515bd885ef7d7748a82168ef
Saurabh Singh <saurabh.rgt@gmail.com>
2024-02-05 23:35:16 +0530

    week3 assignment

--- week-3/03-mongo/routes/admin.js
+++ week-3/03-mongo/routes/admin.js
@@ -1,18 +1,37 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
 const router = Router();
+const { Admin } = require("../db");
 
 // Admin Routes
-router.post('/signup', (req, res) => {
-    // Implement admin signup logic
+router.post("/signup", async (req, res) => {
+  // Implement admin signup logic
+  const username = req.body.username;
+  const password = req.body.password;
+
+  //check username with this user already exists
+  try {
+    await Admin.create({
+      username,
+      password,
+    });
+
+    res.json({
+      msg: "Admin Created Successfully",
+    });
+  } catch (error) {
+    res.status(400).json({
+      msg: "Admin not created",
+    });
+  }
 });
 
-router.post('/courses', adminMiddleware, (req, res) => {
-    // Implement course creation logic
+router.post("/courses", adminMiddleware, (req, res) => {
+  // Implement course creation logic
 });
 
-router.get('/courses', adminMiddleware, (req, res) => {
-    // Implement fetching all courses logic
+router.get("/courses", adminMiddleware, (req, res) => {
+  // Implement fetching all courses logic
 });
 
 module.exports = router;
